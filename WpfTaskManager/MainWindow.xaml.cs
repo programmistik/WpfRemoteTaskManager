14fd43f39c804896fc6d466f8767805f5d422997
Enkeli __ <programmistik@hotmail.com>
2019-02-20 12:20:11 +0400

    Add project files.

---
+++ WpfTaskManager/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//using WpfTaskManager.TaskManagerViewModel;
+
+namespace WpfTaskManager
+{
+    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new TaskManagerViewModel();
+        }
+
+        //GridViewColumnHeader _lastHeaderClicked = null;
+        //ListSortDirection _lastDirection = ListSortDirection.Ascending;
+
+        //void GridViewColumnHeaderClickedHandler(object sender,
+        //                                        RoutedEventArgs e)
+        //{
+        //    var headerClicked = e.OriginalSource as GridViewColumnHeader;
+        //    ListSortDirection direction;
+
+        //    if (headerClicked != null)
+        //    {
+        //        if (headerClicked.Role != GridViewColumnHeaderRole.Padding)
+        //        {
+        //            if (headerClicked != _lastHeaderClicked)
+        //            {
+        //                direction = ListSortDirection.Ascending;
+        //            }
+        //            else
+        //            {
+        //                if (_lastDirection == ListSortDirection.Ascending)
+        //                {
+        //                    direction = ListSortDirection.Descending;
+        //                }
+        //                else
+        //                {
+        //                    direction = ListSortDirection.Ascending;
+        //                }
+        //            }
+
+        //            var columnBinding = headerClicked.Column.DisplayMemberBinding as Binding;
+        //            var sortBy = columnBinding?.Path.Path ?? headerClicked.Column.Header as string;
+
+                   
+        //            Sort(sortBy, direction);
+
+        //            if (direction == ListSortDirection.Ascending)
+        //            {
+        //                headerClicked.Column.HeaderTemplate =
+        //                  Resources["HeaderTemplateArrowUp"] as DataTemplate;
+        //            }
+        //            else
+        //            {
+        //                headerClicked.Column.HeaderTemplate =
+        //                  Resources["HeaderTemplateArrowDown"] as DataTemplate;
+        //            }
+
+        //            // Remove arrow from previously sorted header  
+        //            if (_lastHeaderClicked != null && _lastHeaderClicked != headerClicked)
+        //            {
+        //                _lastHeaderClicked.Column.HeaderTemplate = null;
+        //            }
+
+        //            _lastHeaderClicked = headerClicked;
+        //            _lastDirection = direction;
+        //        }
+        //    }
+        //}
+
+    }
+}
---
+++ WpfTaskManager/TaskManagerViewModel.cs
@@ -0,0 +1,173 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace WpfTaskManager
+{
+    public class TaskManagerViewModel : ViewModelBase
+    {
+        [DllImport("User32.dll")]
+        public static extern Int32 SetForegroundWindow(int hWnd);
+
+        private ObservableCollection<ProcessItem> processCollection;
+        public ObservableCollection<ProcessItem> ProcessCollection { get => processCollection; set => Set(ref processCollection, value); }
+
+        private ProcessItem selItem;
+        public ProcessItem SelItem { get => selItem; set => Set(ref selItem, value); }
+
+        private string startProcess;
+        public string StartProcess { get => startProcess; set => Set(ref startProcess, value); }
+
+        private Timer tm;
+
+        public TaskManagerViewModel()
+        {
+            
+            ProcessCollection = new ObservableCollection<ProcessItem>();
+
+            foreach (var item in Process.GetProcesses("192.168.5.65"))
+            {
+                try
+                {
+                    var newItem = new ProcessItem();
+                    newItem.Pid = item.Id;
+                    newItem.Name = item.ProcessName;
+                   
+                    ProcessCollection.Add(newItem);
+
+                }
+                catch (Exception) { }
+
+                tm = new Timer(LoadProcesses, null, 0, 5000);
+
+            }
+        }
+
+   
+        public void LoadProcesses(object sender)
+        {
+            var newColl = new ObservableCollection<ProcessItem>();
+
+            foreach (var item in Process.GetProcesses("192.168.5.65"))
+            {
+                try
+                {
+                    if (ProcessCollection.Where(i => i.Pid == item.Id).Any())
+                    {
+                        var oldItem = ProcessCollection.Where(i => i.Pid == item.Id).Single();
+                        newColl.Add(oldItem);
+                    }
+                    else
+                    {
+                        var newItem = new ProcessItem();
+                        newItem.Pid = item.Id;
+                        newItem.Name = item.ProcessName;
+                        newColl.Add(newItem);
+                    }
+                }
+                catch (Exception) { }
+            }
+            
+            Application.Current.Dispatcher.Invoke( () =>
+            {
+                ProcessCollection = newColl;
+            });
+
+        }
+
+      
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get => addCommand ?? (addCommand = new RelayCommand(
+                () =>
+                {
+                    if (!String.IsNullOrEmpty(StartProcess))
+                    {
+                        try
+                        {
+
+                            ConnectionOptions theConnection = new ConnectionOptions();
+                            ManagementScope manScope = new ManagementScope("\\\\" + "192.168.5.65" + "\\root\\cimv2", theConnection);
+                            manScope.Connect();
+                            ObjectGetOptions objectGetOptions = new ObjectGetOptions();
+                            ManagementPath managementPath = new ManagementPath("Win32_Process");
+                            ManagementClass processClass = new ManagementClass(manScope, managementPath, objectGetOptions);
+                            ManagementBaseObject inParams = processClass.GetMethodParameters("Create");
+                            inParams["CommandLine"] = StartProcess; //"notepad";
+                            ManagementBaseObject outParams = processClass.InvokeMethod("Create", inParams, null);
+
+
+                            var par2 = outParams["processId"].ToString();
+                            int prId = int.Parse(par2);
+
+                            var proc = Process.GetProcessById(prId, "192.168.5.65");
+                            proc.StartInfo.UseShellExecute = false;
+
+                            //var hwd = proc.MainWindowHandle;
+                            //SetForegroundWindow(hwd.ToInt32());
+
+                            //var handle = Process.GetCurrentProcess().MainWindowHandle;
+                            //Process.Start("notepad.exe").WaitForInputIdle();
+                            //SetForegroundWindow(handle.ToInt32());
+
+
+                            StartProcess = "";
+                            ProcessCollection.Clear();
+                            LoadProcesses(null);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.ToString());
+                        }
+                    }
+                }
+            ));
+        }
+
+        private RelayCommand endCommand;
+        public RelayCommand EndCommand
+        {
+            get => endCommand ?? (endCommand = new RelayCommand(
+                () =>
+                {
+                    if (SelItem == null)
+                        MessageBox.Show("Select any process");
+                    else
+                    {
+                        Process p = Process.GetProcessById(SelItem.Pid, "192.168.5.65");
+                        new Process
+                        {
+                            StartInfo = new ProcessStartInfo
+                            {
+                                FileName = "TaskKill.exe",
+                                Arguments = string.Format("/pid {0} /s {1}", p.Id, p.MachineName),
+                                WindowStyle = ProcessWindowStyle.Hidden,
+                                CreateNoWindow = true
+                            }
+                        }.Start();
+
+                    }
+                }
+            ));
+        }
+
+       
+
+    }
+}
